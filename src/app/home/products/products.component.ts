e28c6c570d2c91aa8ae246b7e08ab3f752c2cee0
mans-18 <miguel.sza@gmail.com>
2021-07-11 20:17:12 -0300

    "adjusted to small screen, nav bars ok"

--- src/app/home/products/products.component.ts
+++ src/app/home/products/products.component.ts
@@ -18,4 +18,27 @@ export class ProductsComponent implements OnInit {
     }
   }
 
+// Used to toggle the menu on smaller screens when clicking on the menu button
+openNav() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// Script for side navigation
+w3_open() {
+  var x = document.getElementById("mySidebar");
+  x.style.width = "300px";
+  x.style.paddingTop = "10%";
+  x.style.display = "block";
+}
+
+// Close side navigation
+w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+
 }
